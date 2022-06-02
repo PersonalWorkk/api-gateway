1049c72876636dc964fa9c68fadb3f2f9c3612f3
simion.razvan <426823@student.fontys.nl>
2022-06-02 21:55:54 +0200

    filter requests based on auth & autho

--- src/main/java/com/wedeliver/apigateway/ApiGatewayApplication.java
+++ src/main/java/com/wedeliver/apigateway/ApiGatewayApplication.java
@@ -1,13 +1,36 @@
 package com.wedeliver.apigateway;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+
+import com.wedeliver.apigateway.config.AuthenticationFilter;
 
 @SpringBootApplication
 public class ApiGatewayApplication {
 
+	@Autowired
+    AuthenticationFilter filter;
+
 	public static void main(String[] args) {
 		SpringApplication.run(ApiGatewayApplication.class, args);
 	}
+
+    @Bean
+    public RouteLocator routes(RouteLocatorBuilder builder){
+        return builder.routes()
+			// Authentication route
+            // Calls the service-account endpoints
+			.route(routeSpec ->
+				routeSpec.path("/api/auth/**")
+					.filters(f -> f.filter(filter))
+					.uri("http://localhost:8081/")
+			)
+			// change uris for production
+			.build();
+    }
 
 }
